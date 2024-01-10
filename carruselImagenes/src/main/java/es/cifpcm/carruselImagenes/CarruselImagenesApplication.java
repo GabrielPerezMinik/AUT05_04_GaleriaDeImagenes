b216e78f6be6b1c00ed6a602a26ee04961c8c82c
gabrielPerezMinik <gabrielc2profe@gmail.com>
2024-01-10 12:58:49 +0000

    Base del proyecto
    Esta es la base mas primitiva del proyecto

---
+++ carruselImagenes/src/main/java/es/cifpcm/carruselImagenes/CarruselImagenesApplication.java
@@ -0,0 +1,13 @@
+package es.cifpcm.carruselImagenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarruselImagenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarruselImagenesApplication.class, args);
+	}
+
+}
---
+++ carruselImagenes/src/test/java/es/cifpcm/carruselImagenes/CarruselImagenesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cifpcm.carruselImagenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarruselImagenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
