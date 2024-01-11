221c1a1b1b504e0c29ee3cf9eebcc3c023dd0534
Alejandro123314 <alejandrojimenezclaro@gmail.com>
2024-01-11 16:28:40 +0000

    PrimerCommit
    1Commit

--- carruselImagenes/src/main/java/es/cifpcm/carruselImagenes/CarruselImagenesApplication.java
+++ carruselImagenes/src/main/java/es/cifpcm/carruselImagenes/CarruselImagenesApplication.java
@@ -1,5 +1,6 @@
 package es.cifpcm.carruselImagenes;
 
+import es.cifpcm.carruselImagenes.controller.ImageManagerServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class CarruselImagenesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarruselImagenesApplication.class, args);
+
 	}
 
 }
